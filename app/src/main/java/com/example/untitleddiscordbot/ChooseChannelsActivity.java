a9be43bac3369f79fe792e2b795b8a850748a5ea
xNikus <comlokoxd@gmail.com>
2022-12-03 04:34:30 +0100

    Add choose channels settings and its cards

---
+++ app/src/main/java/com/example/untitleddiscordbot/ChooseChannelsActivity.java
@@ -0,0 +1,14 @@
+package com.example.untitleddiscordbot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseChannelsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_channels);
+    }
+}
